334903c7cb1778679e19b9a25aa34f5e44a09db4
Hector Diaz <3kthor3adward@gmail.com>
2020-08-13 18:05:23 -0500

    remove unnecessary factories

--- src/main/java/com/raro28/payments/api/rest/configurations/PaymentsConfiguration.java
+++ src/main/java/com/raro28/payments/api/rest/configurations/PaymentsConfiguration.java
@@ -3,8 +3,9 @@ package com.raro28.payments.api.rest.configurations;
 import com.raro28.payments.api.services.ChargeService;
 import com.raro28.payments.api.services.CustomerService;
 import com.raro28.payments.api.services.SourceService;
-import com.raro28.payments.api.services.factories.ServiceFactory;
-import com.raro28.payments.api.services.factories.impl.stripe.ServiceFactoryImpl;
+import com.raro28.payments.api.services.impl.openpay.ChargeServiceImpl;
+import com.raro28.payments.api.services.impl.openpay.CustomerServiceImpl;
+import com.raro28.payments.api.services.impl.openpay.SourceServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,22 +20,17 @@ public class PaymentsConfiguration {
     private OpenpayAPI api;
 
     @Bean
-    public ServiceFactory serviceFactory(){
-        return new ServiceFactoryImpl();
+    public ChargeService chargeService() {
+        return new ChargeServiceImpl(api);
     }
 
     @Bean
-    public ChargeService chargeService(ServiceFactory serviceFactory) {
-        return serviceFactory.getChargeService();
+    public CustomerService customerService() {
+        return new CustomerServiceImpl(api);
     }
 
     @Bean
-    public CustomerService customerService(ServiceFactory serviceFactory) {
-        return serviceFactory.getCustomerService();
-    }
-
-    @Bean
-    public SourceService sourceService(ServiceFactory serviceFactory) {
-        return serviceFactory.getSourceService();
+    public SourceService sourceService() {
+        return new SourceServiceImpl(api);
     }
 }
